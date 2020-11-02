edd93159d83f7d4d02d8b42f27ea4a4ff535568a
Glen M <gmurray@neonsky.net>
2020-11-02 16:27:54 -0600

    Source Changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/dashboard',[TasksController::class, 'index'])->name('dashboard');
+
+    Route::get('/task',[TasksController::class, 'add']);
+    Route::post('/task',[TasksController::class, 'create']);
+
+    Route::get('/task/{task}', [TasksController::class, 'edit']);
+    Route::post('/task/{task}', [TasksController::class, 'update']);
 });
